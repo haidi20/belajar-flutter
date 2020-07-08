e1242aeda5c5d59c1142097697efe32751347ee5
haidi20 <nhaidii@yahoo.com>
2020-07-08 08:51:58 +0800

    berhasil membuat layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
+import "input_page.dart";
 
 void main() => runApp(BMICalculator());
 
@@ -13,27 +12,6 @@ class BMICalculator extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("BMI Calculator"),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Text("hello"),
-        ),
-      ),
     );
   }
 }
